a3339a3167e9c3d89629fdae92022f00d029344a
heroInCommunity <heroin62134@gmail.com>
2013-11-29 17:07:23 +0200

    Add selectize and use it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+$( document ).ready(function() {
+	$('#search-word').selectize({
+	    delimiter: ',',
+	    persist: false,
+	 	plugins: ['restore_on_backspace', 'remove_button'],
+	    create: function(input) {
+	        return {
+	            value: input,
+	            text: input
+	        }
+	    },
+	     maxItems: 12
+	});
+	
+	$('select.selectized,input.selectized').each(function() {
+		var $container = $('<div>').addClass('value').html('Current Value: ');
+		var $value = $('<span>').appendTo($container);
+		var $input = $(this);
+		var update = function(e) { $value.text(JSON.stringify($input.val())); }
+	
+		$(this).on('change', update);
+		update();
+	
+		$container.insertAfter($input.next());
+	});
+
+});
 
