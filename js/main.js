a0db5a538abb6a11359af968261ef52df02ec6dd
heroInCommunity <heroin62134@gmail.com>
2013-12-02 14:20:53 +0200

    Fix styles Make animation to switch between full/less modes for main.html

--- js/main.js
+++ js/main.js
@@ -7,9 +7,55 @@ $( document ).ready(function() {
 	        return {
 	            value: input,
 	            text: input
-	        }
+	        };
 	    },
 	     maxItems: 12
+	});
+	
+	var centralBarWidth = $("#page_central_bar").outerWidth();
+	var leftBarWidth = $("#page_left_bar").outerWidth();
+	
+	$('#clicker_full').click(function() {
+		$(".selectize-input").fadeOut(500);
+		$('#page_left_bar').animate(
+			{
+				width: "5%",
+				opacity: .5
+			},
+			500,
+			"linear"
+		);
+		$("#page_central_bar").animate(
+			{
+				width: "95%"
+			},
+			1000,
+			"linear"
+		);
+		$(this).fadeOut(100);
+		$('#clicker_less').fadeIn(100);
+	});
+	
+	$('#clicker_less').click(function() {
+		$(".selectize-input").fadeIn(1000);
+		$('#page_left_bar').animate(
+			{
+				width: leftBarWidth + "px",
+				opacity: 1
+			},
+			1000,
+			"linear"
+		);
+		$("#page_central_bar").animate(
+			{
+				width: centralBarWidth + "px"
+			},
+			500,
+			"linear"
+		);
+		
+		$(this).fadeOut(100);
+		$('#clicker_full').fadeIn(100);
 	});
 	
 	/*
